fd617d39c3fbfb0a3902998cd253bb1e47569058
Jordy Slagter <Jordy Slagter>
2024-09-19 10:43:43 +0200

    scaffold project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2024_b1_individueel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
