7456948bf64ae2622a71c3d06082d751ed5a42a4
Jordy Slagter <Jordy Slagter>
2024-09-19 13:29:31 +0200

    add flag properties

--- Models/Flag.cs
+++ Models/Flag.cs
@@ -2,4 +2,24 @@
 
 namespace _2024_b1_individueel.Models;
 
-public class Flag : Entity { }
+/// <summary>
+/// Represents a country's flag.
+/// </summary>
+/// <param name="_correctAnswers"></param>
+public class Flag(string countryCode, Lst<string> correctAnswers) : Entity
+{
+    /// <summary>
+    /// Country code for this flag, also used to generate the svg url.
+    /// </summary>
+    public readonly string CountryCode = countryCode;
+
+    /// <summary>
+    /// A list of correct answers for this flag.
+    /// </summary>
+    public readonly Lst<string> CorrectAnswers = correctAnswers;
+
+    /// <summary>
+    /// The URL for the SVG file of this flag (from flagcdn.com).
+    /// </summary>
+    public string ImageURL => $"https://flagcdn.com/{CountryCode}.svg";
+}
